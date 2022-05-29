7e19e6c849e066e449d0da27389bbd52ea582a3e
Atul <aksingh.raickwar@gmail.com>
2022-05-29 22:45:38 +0530

    Add files via upload

---
+++ program19.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    //char name[10], ace[12];
+    //char z=cin.get();
+    //cin.getline(name, 6);
+    //cin.getline(ace, 8);
+    //cout.put("name");
+    //cout.write(name, 4).write(ace, 5);
+    //char a[10]="Solar";
+    //cout<<setw(10);
+    //cout<<setfill('*')<<a<<endl;
+    //float h=12.356489;
+    //cout<<setprecision(4)<<h<<endl;
+    cout<<hex;
+    cout<<setiosflags(ios::showbase)<<200<<endl;
+    cout<<resetiosflags(ios::showbase)<<200<<endl;
+    return 0;
+    
+}
